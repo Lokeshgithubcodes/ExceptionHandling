e9e48656f6ff569e0ca800dc30ca8b6d1aef3d3f
Lokeshgithubcodes <ramisettylokesh642@gmail.com>
2024-02-09 22:25:02 +0530

    first commit

---
+++ ExceptionDemo/Demo1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class DivideByOddNoException : ApplicationException
+    {
+        public override string Message
+        {
+            get
+            {
+                return "attemted by odd no";
+            }
+
+        }
+    }
+    public class Demo1
+    {
+        static void Main(string[] args) 
+        {
+
+
+            try
+            {
+                Console.WriteLine("Enter the 1st Num:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the 2nd Num");
+                int b = int.Parse(Console.ReadLine());
+                if(b%2==1)
+                {
+                   // throw new ApplicationException("Divisor can't be a odd number");
+                   throw new DivideByOddNoException();
+                }
+                int res = a / b;
+                Console.WriteLine("The result=" + res);
+                
+            }
+            catch(FormatException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block Executed");
+            }
+            Console.WriteLine("End of the program");
+        }
+    }
+}
---
+++ ExceptionDemo/Demo2.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ExceptionDemo
+//{
+//    public class Demo2
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = {3,6,3,7,9,4,5,2,6,7,8};
+//            int count = 0;
+//            for(int i=0;i<arr.Length;i++)
+//            {
+//                if (arr[i] > 2)
+//                {
+//                    count++;
+//                }
+//            }
+//            int[] brr= new int[count];
+//            int index = 0;
+//            for (int i = 0; i < brr.Length; i++)
+//            {
+//                if (brr[i] > 2)
+//                {
+//                    index++;
+//                }
+//            }
+//            Array.Sort(brr);
+//            Array.Reverse(brr);
+
+//            Console.Write(brr + " ");
+
+//        }
+//    }
+//}
